c7f9bb9c4f758205477c770f997a9c1868ddbade
sagar <sagark.kale@hotmail.com>
2019-12-21 19:01:06 +0530

    feature : added misc api which return stats feature : added customer type : wholesaler & customer

---
+++ src/main/java/com/sagar/controller/Miscellaneous.java
@@ -0,0 +1,44 @@
+package com.sagar.controller;
+
+import com.sagar.repository.CustomerRepository;
+import com.sagar.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+@RestController
+@RequestMapping("/misc")
+@Slf4j
+public class Miscellaneous {
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public ResponseEntity<?> getMiscData() {
+        Map map = new LinkedHashMap();
+        map.put("customerCount", customerRepository.count());
+        BigDecimal sum = new BigDecimal(0);
+        final List<BigDecimal> bigDecimalList = productRepository.findAll()
+                .stream()
+                .map(product -> product.getPurchasePrice().multiply(BigDecimal.valueOf(product.getQty())))
+                .collect(Collectors.toList());
+        for (BigDecimal b : bigDecimalList) {
+            sum = sum.add(b);
+        }
+        map.put("totalLoss", sum);
+        return ResponseEntity.ok(map);
+    }
+}
--- src/main/java/com/sagar/entity/Customer.java
+++ src/main/java/com/sagar/entity/Customer.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @ToString
 public class Customer extends CommonFields {
+    enum  type {
+        CUSTOMER, WHOLESALER
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,4 +21,6 @@ public class Customer extends CommonFields {
     private String address;
     private String phone;
     private String email;
+    @Enumerated(EnumType.STRING)
+    private type type;
 }
